df290be644f8263a914543f7beb38bdf6db67f20
danilaKazimir <kazimird99@gmail.com>
2025-01-30 01:18:53 +0400

    fix/fix-faker

--- src/models/booking/create_booking_request.py
+++ src/models/booking/create_booking_request.py
@@ -1,17 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.utils.faker_generate import FakerGenerate
 
 
 class BookingDates(BaseModel):
-    checkin: str = FakerGenerate.fake.date_between(start_date='-7d', end_date='-1d')
-    checkout: str = FakerGenerate.fake.date_between(start_date='today', end_date='today')
+    checkin: str = Field(default_factory=lambda: FakerGenerate.generate_date(start_date='-7d', end_date='-3d'))
+    checkout: str = Field(default_factory=lambda: FakerGenerate.generate_date(start_date='today', end_date='today'))
 
 
 class CreateBookingRequest(BaseModel):
-    firstname: str = FakerGenerate.generate_first_name()
-    lastname: str = FakerGenerate.generate_last_name()
-    totalprice: int = FakerGenerate.generate_total_price()
-    depositpaid: bool = FakerGenerate.generate_is_deposit_paid()
-    bookingdates: BookingDates = BookingDates()
-    additionalneeds: str = FakerGenerate.generate_additional_needs()
+    firstname: str = Field(default_factory=FakerGenerate.generate_first_name)
+    lastname: str = Field(default_factory=FakerGenerate.generate_last_name)
+    totalprice: int = Field(default_factory=FakerGenerate.generate_total_price)
+    depositpaid: bool = Field(default_factory=FakerGenerate.generate_is_deposit_paid)
+    bookingdates: BookingDates = Field(default_factory=BookingDates)
+    additionalneeds: str = Field(default_factory=FakerGenerate.generate_additional_needs)
