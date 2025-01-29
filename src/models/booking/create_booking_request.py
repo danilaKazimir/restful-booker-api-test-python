373f0264f1670c517fef294a55c3c26ff37fc44a
danilaKazimir <kazimird99@gmail.com>
2025-01-30 00:58:20 +0400

    feature/add-faker

--- src/models/booking/create_booking_request.py
+++ src/models/booking/create_booking_request.py
@@ -1,15 +1,17 @@
 from pydantic import BaseModel
 
+from src.utils.faker_generate import FakerGenerate
+
 
 class BookingDates(BaseModel):
-    checkin: str = '2018-01-01'
-    checkout: str = '2019-01-01'
+    checkin: str = FakerGenerate.fake.date_between(start_date='-7d', end_date='-1d')
+    checkout: str = FakerGenerate.fake.date_between(start_date='today', end_date='today')
 
 
 class CreateBookingRequest(BaseModel):
-    firstname: str = 'Jim'
-    lastname: str = 'Brown'
-    totalprice: int = 111
-    depositpaid: bool = True
+    firstname: str = FakerGenerate.generate_first_name()
+    lastname: str = FakerGenerate.generate_last_name()
+    totalprice: int = FakerGenerate.generate_total_price()
+    depositpaid: bool = FakerGenerate.generate_is_deposit_paid()
     bookingdates: BookingDates = BookingDates()
-    additionalneeds: str = 'Breakfast'
+    additionalneeds: str = FakerGenerate.generate_additional_needs()
